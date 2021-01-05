7f9eec5e63a2e8f9fbf6a1bf664bef17d3f24e35
unknown <chris_amantino97@hotmail.com>
2021-01-05 13:32:37 -0300

    tratativa de erros

--- DesafioOrangeTalents/src/main/resources/static/controller.js
+++ DesafioOrangeTalents/src/main/resources/static/controller.js
@@ -6,20 +6,25 @@ app.controller('myCtrl', function($scope, $http) {
 
 
 $scope.cadastrar = function() {
-	/*let nome = document.querySelector('#nome')
+	let nome = document.querySelector('#nome')
 	let email = document.querySelector('#email')
 	let cpf = document.querySelector('#cpf')
-	let data = document.querySelector('#data')*/
+	let data = document.querySelector('#dataNascimento')
 	
+	if(nome.value == '' || email.value == '' || cpf.value == '' || data.value == ''){
+		Swal.fire('Ops!', 'Preencha todos os campos!','warning')
+	} else {
   $http.post("http://localhost:8080/usuario/cadastro", {
     'nome':$scope.nome,
     'email':$scope.email,
     'cpf':$scope.cpf,
     'dataNascimento':$scope.dataNascimento
     
+  }).then(resp => {
+	  Swal.fire('Deu tudo certo!', 'Usuário cadastrado com sucesso!','success')
   }).catch(error => {
-	  console.log(error)
-  })
+		Swal.fire('Houve um problema!', 'Usuário já cadastrado! Verifique se seu email ou CPF já foram cadastrados.','error')
+  })}
   
 };
 
